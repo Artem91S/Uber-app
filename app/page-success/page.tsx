521bf1e2af259d1f298f3472cc8f45231e40b16e
Артём Ситников <sitnikov.artem91@gmail.com>
2023-11-11 15:32:22 +0200

    minor changes

--- app/page-success/page.tsx
+++ app/page-success/page.tsx
@@ -1,17 +1,19 @@
-
+"use-client"
 import Image from "next/image"
 import Link from "next/link"
 
 function SuccessPayment() {
 
-    
   return (
-    <div className='flex w-full flex-col items-center'>
+    <div className='flex w-full flex-col items-center relative'>
     <Image src='/uberconfirm.gif' alt='confirm order gift' width={500} height={500} className="w-full" />
+    <div className='absolute'>
     <p className='text-center pt-4 font-bold'>You success book the car</p>
     <Link className='text-center p-4 cursor-pointer'href='/'>
     <button className='p-2 bg-black text-white rounded-xl text-center'>Go to home page</button>
+    
     </Link>
+    </div>
     </div>
   )
 }
