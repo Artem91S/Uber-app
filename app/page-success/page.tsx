12fd0f406207690826aa183911372ea66c167dca
Артём Ситников <sitnikov.artem91@gmail.com>
2023-11-11 13:15:29 +0200

    add page success payment

---
+++ app/page-success/page.tsx
@@ -0,0 +1,19 @@
+
+import Image from "next/image"
+import Link from "next/link"
+
+function SuccessPayment() {
+
+    
+  return (
+    <div className='flex w-full flex-col items-center'>
+    <Image src='/uberconfirm.gif' alt='confirm order gift' width={500} height={500} className="w-full" />
+    <p className='text-center pt-4 font-bold'>You success book the car</p>
+    <Link className='text-center p-4 cursor-pointer'href='/'>
+    <button className='p-2 bg-black text-white rounded-xl text-center'>Go to home page</button>
+    </Link>
+    </div>
+  )
+}
+
+export default SuccessPayment
