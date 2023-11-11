21cba1e6d981863823adfd207f4c7650dabad296
Артём Ситников <sitnikov.artem91@gmail.com>
2023-11-11 13:14:52 +0200

    add payment page

---
+++ app/payment/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import CheckOutForm from "../components/Payment/CheckOutForm";
+import { useSearchParams } from "next/navigation";
+
+function Payment() {
+  const params = useSearchParams();
+  const amountOfRide = params.get("amount");
+  const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY!);
+  const payment = {
+    mode: "payment",
+    amount: Math.round(Number(amountOfRide) * 100),
+    currency: "uah",
+  };
+  return (
+    <Elements stripe={stripe} options={payment}>
+      <CheckOutForm amount={amountOfRide} />
+    </Elements>
+  );
+}
+
+export default Payment;
