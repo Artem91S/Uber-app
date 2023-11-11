60f110bdc46cc200916b543aa91cc9b107495166
Артём Ситников <sitnikov.artem91@gmail.com>
2023-11-11 15:32:35 +0200

    minor changes

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -4,14 +4,14 @@ import { StripeElementsOptionsMode, loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import CheckOutForm from "../components/Payment/CheckOutForm";
 import { useSearchParams } from "next/navigation";
-import { POST } from "../api/create-intent/route";
+
 
 function Payment() {
   const params = useSearchParams();
   const amountOfRide = params.get("amount")!;
   const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY!);
-  const options:StripeElementsOptionsMode | null = {
-    mode:"payment",
+  const options:StripeElementsOptionsMode  = {
+    mode:'payment',
     amount: Math.round(Number(amountOfRide) * 100),
     currency: 'uah',
   };
