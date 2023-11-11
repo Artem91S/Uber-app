fb06826f49a33ed84d09d333d3867266f143a213
Артём Ситников <sitnikov.artem91@gmail.com>
2023-11-11 13:17:02 +0200

    change mock data

---
+++ mock/CarsData.js
@@ -0,0 +1,43 @@
+export const carsData=[
+    {
+      id:1,
+      name:'Uber X',
+      seat:4,
+      dest:'Affordable,Everyday rides',
+      amount:40,
+      image:'/UberX_v1.png'
+    },
+    {
+        id:2,
+        name:'Comfort',
+        seat:4,
+        dest:'Newer cars with extra legroom',
+        amount:44,
+        image:'/UberComfort.png'
+      },
+      {
+        id:3,
+        name:'Uber XL',
+        seat:6,
+        dest:'Affodable ride for groups up to 6',
+        amount:48,
+        image:'/UberXL_New.png'
+      },
+      {
+        id:4,
+        name:'Uber Pet',
+        seat:4,
+        dest:'Affordable rides for you and your pet',
+        amount:47,
+        image:'/UberX_Pet.png'
+      },
+      {
+        id:5,
+        name:'Black',
+        seat:4,
+        dest:'Affordable,Everyday rides',
+        amount:55,
+        image:'/Black_v1.png'
+      },
+
+]
