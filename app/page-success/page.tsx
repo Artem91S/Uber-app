35491ebe55c637b20f30b37f35475977aa1a6f8d
Артём Ситников <sitnikov.artem91@gmail.com>
2023-11-12 11:08:47 +0200

    add placeholder img

--- app/page-success/page.tsx
+++ app/page-success/page.tsx
@@ -6,7 +6,15 @@ function SuccessPayment() {
 
   return (
     <div className='flex w-full flex-col items-center relative'>
-    <Image src='/uberconfirm.gif' alt='confirm order gift' width={500} height={500} className="w-full" />
+    <Image 
+    src='/uberconfirm.gif' 
+    alt='confirm order gift' 
+    width={500} 
+    height={500} 
+    className="w-full"
+    placeholder="data:image/badePhoto.png"
+    
+    />
     <div className='absolute'>
     <p className='text-center pt-4 font-bold'>You success book the car</p>
     <Link className='text-center p-4 cursor-pointer'href='/'>
