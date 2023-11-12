aa77a55097f177027a3d444b81c0af4b5dcb6f97
Артём Ситников <sitnikov.artem91@gmail.com>
2023-11-12 11:09:39 +0200

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,66 @@
 "use client"
 import SearchForm from "./components/Home/SearchForm";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import GoogleMapSection from "./components/Home/GoogleMap";
 import { LoadScript } from "@react-google-maps/api";
-export type FieldTypes = {
-  lat: number;
-  lng: number;
-  name: string;
-  label: string;
-} ;
+import { FieldTypes } from "@/type";
 
+type SuccessTypes={
+  coords:{
+    latitude:number;
+    longitude:number;
+  }
+}
+const initialValue = {
+  lat: 0,
+  lng: 0,
+  name: '',
+  label:''
+}
 
 export default function Home() {
-  const [source, setSource] = useState<FieldTypes>(
-    {
-          lat: 0,
-          lng: 0,
-          name: '',
-          label:''
-        }
-  );
-  const [destination, setDestination] = useState<FieldTypes>(
+  const [source, setSource] = useState<FieldTypes>(initialValue);
+  const [destination, setDestination] = useState<FieldTypes>(initialValue);
+useEffect(()=>{
+  handleLocationOfUser()
 
-    {
-          lat: 0,
-          lng: 0,
-          name: '',
-          label:''
-        }
-  );
+},[])
+  const handleLocationOfUser =()=>{
+    if(navigator.geolocation){
+     navigator.geolocation.getCurrentPosition(success)
+    }
+    else{
+      console.log("Location is off");
+      
+    }
+  }
+
+  const success =(position:SuccessTypes)=>{   
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+  setSource( {
+      lat:lat,
+      lng:lng,
+      name: 'My location',
+      label:'Me'
+    })
+  }
+    
 
   return (
-        <LoadScript
-        libraries={['places']}
-        googleMapsApiKey='AIzaSyChvhc0pkiHubul1Na-eHbYBB3sCtkUuB4'
-        >
-        <div className="m-3 grid grid-cols-1 md:grid-cols-3">
-          <div >
-            <SearchForm source={source} setSource={setSource} destination={destination} setDestination={setDestination}/>
-          </div>
-          <div className=" col-span-2">
-            <GoogleMapSection source={source}  destination={destination} /> 
-          </div>
+ <LoadScript
+      libraries={['places']}
+      googleMapsApiKey='AIzaSyChvhc0pkiHubul1Na-eHbYBB3sCtkUuB4'
+      >
+      <div className="m-3 grid grid-cols-1 md:grid-cols-3">
+        <div >
+          <SearchForm source={source} setSource={setSource} destination={destination} setDestination={setDestination}/>
         </div>
-        </LoadScript>
+        <div className=" col-span-2">
+          <GoogleMapSection source={source}  destination={destination} /> 
+        </div>
+      </div>
+      </LoadScript>
+ 
   );
 }
